709547723615abb33dcedeba2b9dfca17e060ea1
Srilekha25 <kasaragaddasrilekha@gmail.com>
2022-12-31 11:33:05 +1100

    changes added

---
+++ Javascript/clock.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector('#section_clock_align');
+
+function updateClock() {
+  const now = new Date();
+  clockContainer.innerHTML = now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000); // Update the clock every second
+
+const menuContainer = document.querySelector('#section_menu_flex');
+
+
+const menuButton = document.getElementById("button_align");
+    const menu = document.getElementById("section_menu_flex");
+
+    menuButton.addEventListener("click", () => {
+      menu.style.display = menu.style.display === "none" ? "flex" : "none";
+    });
