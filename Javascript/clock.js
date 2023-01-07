c501b23f21dd1acdf525d4d8b18770202bbe64cc
Srilekha25 <kasaragaddasrilekha@gmail.com>
2023-01-07 14:20:50 +1100

    textarea added

--- Javascript/clock.js
+++ Javascript/clock.js
@@ -1,4 +1,4 @@
-const clockContainer = document.querySelector('#section_clock_align');
+const clockContainer = document.querySelector("#section_clock_align");
 
 function updateClock() {
   const now = new Date();
@@ -7,17 +7,26 @@ function updateClock() {
 
 setInterval(updateClock, 1000); // Update the clock every second
 
-const menuContainer = document.querySelector('#section_menu_flex');
+const menuContainer = document.querySelector("#section_menu_flex");
 const menuButton = document.getElementById("button_align");
-    const menu = document.getElementById("section_menu_flex");
+const menu = document.getElementById("section_menu_flex");
 
-    menuButton.addEventListener("click", () => {
-      menu.style.display = menu.style.display === "none" ? "flex" : "none";
-    });
+menuButton.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "none" ? "flex" : "none";
+});
 
-    const icon = document.getElementById('icon-1');
-    const section = document.getElementById('section__notepad--textarea');
-    
-    icon.addEventListener('click', function() {
-     section.style.display = section.style.display === 'none' ? "flex" : "none";
-    });
+// const notepad = document.getElementById('icon-1');
+// const notepadSection = document.getElementById('section__notepad--textarea');
+
+// notepad.addEventListener('click', function() {
+//   notepadSection.style.display = notepadSection.style.display === 'none' ? "flex" : "none";
+// });
+
+// const tempConvertor = document.getElementById('icon-2');
+// const sectionTempConvertor = document.getElementById('section__temp-convertor--form');
+
+// tempConvertor.addEventListener('click', function() {
+// sectionTempConvertor.style.display = sectionTempConvertor.style.display === 'none' ? "flex" : "none";
+// });
+
+
