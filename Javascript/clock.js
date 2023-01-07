b3557aaab31e6db59c57f1a2b1e55d388aaa0805
Srilekha25 <kasaragaddasrilekha@gmail.com>
2023-01-08 00:17:53 +1100

    temp convertor

--- Javascript/clock.js
+++ Javascript/clock.js
@@ -7,7 +7,7 @@ function updateClock() {
 
 setInterval(updateClock, 1000); // Update the clock every second
 
-const menuContainer = document.querySelector("#section_menu_flex");
+//To display container for start button
 const menuButton = document.getElementById("button_align");
 const menu = document.getElementById("section_menu_flex");
 
@@ -15,18 +15,73 @@ menuButton.addEventListener("click", () => {
   menu.style.display = menu.style.display === "none" ? "flex" : "none";
 });
 
-// const notepad = document.getElementById('icon-1');
-// const notepadSection = document.getElementById('section__notepad--textarea');
+const appendElementWithText = (elementType, text, parentElement) => {
+  const createdElement = document.createElement(elementType);
+  const textNode = document.createTextNode(text);
+  createdElement.appendChild(textNode);
+  parentElement.appendChild(createdElement);
+};
 
-// notepad.addEventListener('click', function() {
-//   notepadSection.style.display = notepadSection.style.display === 'none' ? "flex" : "none";
-// });
+//start of temperature convertor
+let messageArr = [];
+const form = document.querySelector(".section__temp-convertor--form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-// const tempConvertor = document.getElementById('icon-2');
-// const sectionTempConvertor = document.getElementById('section__temp-convertor--form');
+  const temperatureInput = document.querySelector("#Temperature").value.trim();
+  const temperatureUnit = document.querySelector("#Convertor").value;
 
-// tempConvertor.addEventListener('click', function() {
-// sectionTempConvertor.style.display = sectionTempConvertor.style.display === 'none' ? "flex" : "none";
-// });
+  if (isNaN(temperatureInput) || temperatureInput <= 0) {
+    messageArr.push(
+      `${temperatureInput} is not equal to number or less than zero. Please enter correct values`
+    );
+    appendElementWithText("div", messageArr, message);
+  } else if (temperatureUnit === "fahrenheit") {
+    const temperatureInCelcius = fahrenheitToCelcius(temperatureInput);
+    messageArr.push(
+      `${temperatureInput}°F is equivalent to ${temperatureInCelcius}°C`
+    );
+    appendElementWithText("div", messageArr, message);
+  } else if (temperatureUnit === "celcius") {
+    const temperatureInFahrenheit = celciusToFahrenheit(temperatureInput);
+    messageArr.push(
+      `${temperatureInput}°C is equivalent to ${temperatureInFahrenheit}°F`
+    );
+    appendElementWithText("div", messageArr, message);
+  }
+  document.querySelector("#Temperature").value = "";
+  const deleteEverything = document.querySelector("#message");
+});
+function fahrenheitToCelcius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
 
+function celciusToFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
+//End of Temperature Convertor
 
+//Display the temperature convertor when icon is clicked
+let TempConvertorIcon = document.getElementById("icon-1");
+let tempConvertorModal = document.getElementById(
+  "section__temp-convertor--form"
+);
+TempConvertorIcon.addEventListener("click", () => {
+  console.log("coming in on icon once clicked");
+  tempConvertorModal.classList.toggle("section__temp-convertor--form--display-mode");
+});
+
+//Close the temperature convertor when X is clicked
+document.getElementById("span__close--align").addEventListener("click", () => {
+  tempConvertorModal.style.display = "none";
+});
+
+//Display nothing when clicked on the screen
+
+var onTheScreen = document.querySelector("img");
+window.onclick = function (event) {
+  if (event.target == onTheScreen) {
+    menu.style.display = "none";
+    modal.style.display = "none";
+  }
+};
--- Javascript/temperature-convertor.js
+++ Javascript/temperature-convertor.js
@@ -1,15 +1,77 @@
-function celciusToFahrenheit(){
-    let celcius = document.getElementById('celcius').value;
-    let fahrenheit = (celcius* 9/5) + 32;
-    document.getElementById('fahrenheit').value = fahrenheit;
+const appendElementWithText = (elementType, text, parentElement) => {
+  const createdElement = document.createElement(elementType);
+  const textNode = document.createTextNode(text);
+  createdElement.appendChild(textNode);
+  parentElement.appendChild(createdElement);
+};
 
+let messageArr = [];
+const form = document.querySelector(".section__temp-convertor--form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const temperatureInput = document.querySelector("#Temperature").value.trim();
+  const temperatureUnit = document.querySelector("#Convertor").value;
+
+  if (isNaN(temperatureInput) || temperatureInput <= 0) {
+    messageArr.push(
+        `${temperatureInput} is not equal to number or less than zero. Please enter correct values`
+      );
+      appendElementWithText("div", messageArr, message);
+  } else if (temperatureUnit === "fahrenheit") {
+    const temperatureInCelcius = fahrenheitToCelcius(temperatureInput);
+    messageArr.push(
+      `${temperatureInput}°F is equivalent to ${temperatureInCelcius}°C`
+    );
+    appendElementWithText("div", messageArr, message);
+  } else if (temperatureUnit === "celcius") {
+    const temperatureInFahrenheit = celciusToFahrenheit(temperatureInput);
+    messageArr.push(
+      `${temperatureInput}°C is equivalent to ${temperatureInFahrenheit}°F`
+    );
+    appendElementWithText("div", messageArr, message);
+  }
+  document.querySelector('#Temperature').value = '';
+  const deleteEverything = document.querySelector('#message');
+
+//   console.log(deleteEverything);
+
+//   deleteEverything.parentNode.removeChild(deleteEverything);
+//   if (deleteEverything) {
+//     message.removeChild(deleteEverything);
+//   }
+
+});
+function fahrenheitToCelcius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
 }
 
-function fahrenheitToCelcius(){
-    let fahrenheit = document.getElementById('fahrenheit').value;
-    let celcius = (fahrenheit - 32) * 5/9
-    document.getElementById('celcius').value = celcius;
+function celciusToFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
 }
 
+// let icon = document.getElementById("icon-1");
+// let btnfortest = document.querySelector("fortest");
 
-const temConvertorContainer = document.querySelector("section__temp-convertor--form");
+let modal = document.getElementById("section__temp-convertor--form");
+
+let icon1 = document.getElementById("icon-1");
+
+var span = document.getElementsByClassName("span__close--align")[0];
+
+icon1.onclick = function() {
+    console.log("coming in");
+    modal.style.display = "flex";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
